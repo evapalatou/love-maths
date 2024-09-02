b1e4a76a5e9f361250cc11bde31e1cc36d2faf9c
Evanthia Palatou <p7dt68xx@students.codeinstitute.net>
2024-09-02 12:43:49 +0000

    Add code for subtraction game

--- 04-multiplication-subtraction-questions/4.1-multiplication-game/assets/js/script.js
+++ 04-multiplication-subtraction-questions/4.1-multiplication-game/assets/js/script.js
@@ -33,8 +33,6 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    } else if (gameType === "subtract" ) {
-        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -78,8 +76,6 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else if (operator === "-") {
-        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -115,11 +111,7 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-function displaySubtractQuestion(operand1, operand2) {
-
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
-    document.getElementById('operator').textContent = "-";
+function displaySubtractQuestion() {
 
 }
 
